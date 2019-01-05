bb15936c0931daaa7d1f6e430626ac115dc5b1f5
Cameron <cameronboyce93@gmail.com>
2019-01-05 12:45:04 -0500

    multiple wildcard functionality added to file selection viewer for file selections w prefix.

--- util/file_selection.py
+++ util/file_selection.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """ This file contains subs related to file selection. Calls will be
     from the static/iter option editor windows
     """
 import subprocess as SP
+from copy import deepcopy
 import re
 import pdb
 
@@ -11,7 +12,7 @@ def list_file_selection_contents(builder, project_vars, fs, ftype):
     fs, fs_arr, match = parse_file_selection(fs, project_vars)
     cmd = "ls -1"
     if not ftype:
-        # add directory flag
+        # add directory flag for directory type selections
         cmd += "d"
     cmd += " " + fs
     cp = SP.run([cmd], stdout=SP.PIPE, stderr=SP.PIPE, shell=True)
@@ -28,51 +29,63 @@ def list_file_selection_contents(builder, project_vars, fs, ftype):
         if len(match) > 0:
             # collect newly concatenated selection in list
             concat_fs = []
-            pdb.set_trace()
+            # iter through shell output of files which already exist
+            # and add user prefix
             for fp in stdout.split("\n"):
                 concat_fs.append(concatenate(fp, fs_arr))
-            
+            # print to buffer
             stdout = print_buffer(concat_fs)
-                        
         builder.get_object('file_selection_buffer').set_text(stdout, -1)   
         builder.get_object('file_selection_viewer').show()
 
 def parse_file_selection(file_selection, variables):
-    # find concatenations
-    #match = re.findall(r'\<(.*?)\>', file_selection)
-    # find concatenations with <> enclosures
+    # find prefixes
+    # find prefix with <> enclosure
     match = re.findall(r'(\<.*?\>)', file_selection)
-    
     # replace any project variable strings with associated value pair
     for var in variables:
         file_selection = file_selection.replace(var[0], var[1])
-    # remove the concatenation string from the file selection since files
+    # remove the prefix string from the file selection since files
     # do not yet exist
     fs_arr = []
-    fs = file_selection
     if len(match) > 0:
-        for i,m in enumerate(match):
-            if i > 0:
-                fs = list(fs_arr[3*i-1].partition(m))
-                fs_arr.remove(fs_arr[-1])
-                fs_arr = fs_arr + list(fs)
-            else:
-                fs_arr = list(file_selection.partition(m))
-            file_selection = file_selection.replace(m, "")       
+        match = match[0]
+        fs_arr = list(file_selection.partition(match))
+        file_selection = file_selection.replace(match, "")       
     return (file_selection, fs_arr, match)
 
 def concatenate(fp, fs_arr):
+    """ Return concatenated string """
+    arr = deepcopy(fs_arr)
+    arr = remove_wildcards(fp, arr)
     to_concat = []
-    for i,fs in enumerate(fs_arr):
-        if i == len(fs_arr)-1:
+    for i,fs in enumerate(arr):
+        if i == 2:
             return  "".join(str(y) for y in to_concat)
         if i % 2 != 0:
-            tup = fp.partition(fs_arr[i-1])
-            if tup[2] != '':
-                to_concat.append(tup[1])
-                to_concat.append(tup[2])
+            tup = fp.partition(arr[i-1])
+            to_concat.append(tup[1])
+            to_concat.append(tup[2])
             to_concat.insert(i, fs[1:-1])
-            fp = to_concat[i+1]
+
+def remove_wildcards(fp, fs_arr):
+    # find the all * chars and prepend with . and append ?
+    # so we get .*? which can be used in regex to populate with
+    # contents outputted by the shell
+    # dont care about prefix so skip it
+    for j in [0,2]:
+        regex = fs_arr[j]
+        no_of_wc = 0
+        for i, char in enumerate(fs_arr[j]):
+            if char == "*":
+                regex = regex[:i+2*no_of_wc] + ".*?" + regex[i+2*no_of_wc+1:]
+                no_of_wc += 1
+        fs_arr[j] = regex
+    m = re.findall(fs_arr[0], fp)
+    fs_arr[0] = m[0]
+    filename = fp.replace(fs_arr[0],"")
+    fs_arr[2] = filename
+    return fs_arr
 
 def print_buffer(arr):
     x = ""
